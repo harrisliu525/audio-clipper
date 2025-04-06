6da7cdd0a2e9baadbaad761e2c2e30a32e10945b
harrisliu525 <harrislhr@163.com>
2025-04-06 21:21:26 +0800

    第六版

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import WaveSurfer from 'https://unpkg.com/wavesurfer.js@7.7.3/dist/wavesurfer.esm.js';
-import RegionsPlugin from 'https://unpkg.com/wavesurfer.js@7.7.3/dist/plugins/regions.esm.js';
+import WaveSurfer from 'wavesurfer.js';
+import RegionsPlugin from 'wavesurfer.js/dist/plugins/regions.js';
 
 let wavesurfer = null;
 let audioContext = null;
